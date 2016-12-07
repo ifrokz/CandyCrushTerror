f5f0d77bdd1895f73b7c3cf92d086e0d0f7f5dd2
Ivan <dyfrok@gmail.com>
2016-12-07 19:16:12 +0100

    updates.txt

--- js/class/cCandy.js
+++ js/class/cCandy.js
@@ -12,13 +12,21 @@ var Candy = function(row,column,type){
 
 
     this.update = function(){ // de momento implementado solamente que caigan pabajo
-        if( (10 + this.y ) < (1910 - this.row * 100) ){
+        if(this.y < this.row*100+100*3+10){
+            this.speed += this.aceleration;
+            this.y += this.speed;
+        }else{
+            this.speed = 0;
+            this.y = this.row*100+100*3+10;
+        }
+        // ESTA ES LA FUNCIÓN ANTIGUA DE JOSE
+        /*if( (10 + this.y ) < (1910 + this.row * 100) ){
             this.speed += this.aceleration;
             this.y += this.speed;
         }else{
             this.speed = 0;
             this.y = 1910 - this.row *100;
-        }
+        }*/
     }
 
     this.render = function(){
@@ -35,11 +43,21 @@ function updateCandies(){
     }
 }
 
-function clickCandies(){ // esta función nos servirá para cuando hagamos click en un caramelo, pillar su row y su column y filtrar en la base de datos para seleccionar ese caramelo
+// ESTA ES LA FUNCIÓN ANTIGUA DE JOSE
+/*function clickCandies(){ // esta función nos servirá para cuando hagamos click en un caramelo, pillar su row y su column y filtrar en la base de datos para seleccionar ese caramelo
     $("#canvas").click(function(event){ // de momento genera caramelos donde haces click
         console.log(event.pageX + " | " + event.pageY);
         if(event.pageX > 40 && event.pageX < 1040 && event.pageY > 10+3*100 && event.pageY < 1910-5*100){
             candies.push(new Candy( 19 - Math.floor( ( event.pageY -10 ) / 100 ) , Math.floor( (event.pageX -40) / 100 ) , Math.ceil(Math.random() * 8 ) ) )
+        }
+    });
+}*/
+
+function clickCandies(){ // esta función nos servirá para cuando hagamos click en un caramelo, pillar su row y su column y filtrar en la base de datos para seleccionar ese caramelo
+    $("#canvas").click(function(event){ // de momento genera caramelos donde haces click
+        console.log(event.pageX + " | " + event.pageY);
+        if(event.pageX > 40 && event.pageX < 1040 && event.pageY > 10+3*100 && event.pageY < 1910-5*100){
+            candies.push(new Candy(Math.floor((event.pageY-10)/100)-3,Math.floor((event.pageX-40)/100),Math.ceil(Math.random()*8)))
         }
     });
 }
